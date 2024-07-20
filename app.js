9b1f1f397888a218686ec6a1742247ddbb533cf6
miguelruizc <ruizconde.m@gmail.com>
2024-07-20 18:16:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { router: indexRouter } = require('./routes/index');
 const newRouter = require('./routes/new');
 
+const port = process.env.PORT;
+
 const app = express();
 
 app.use(express.static('public'));
@@ -15,4 +17,4 @@ app.use((req, res) => {
 	res.status(404).render('404');
 });
 
-app.listen(3000, () => console.log('Server running, listening to port 3000'));
+app.listen(port, () => console.log(`Server running, listening to port ${port}`));
