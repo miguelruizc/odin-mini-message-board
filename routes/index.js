83dcdaeb0324d1fd5e22b146b27db3aba4504749
miguelruizc <ruizconde.m@gmail.com>
2024-07-21 17:15:24 +0200

    Order message board by creation time

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const pool = require('../db/pool');
 
 router.get('/', async (req, res) => {
-	const messagesRaw = await pool.query('SELECT * FROM messages');
+	const messagesRaw = await pool.query(
+		'SELECT * FROM messages ORDER BY created_at DESC'
+	);
 	const messages = messagesRaw.rows;
 	res.status(200).render('index', { title: 'Messages', messages });
 });
