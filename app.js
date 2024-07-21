5fa7ce50ffb3dfb9c0e1cd8467582c48ccb5be0b
miguelruizc <ruizconde.m@gmail.com>
2024-07-21 13:07:51 +0200

    Add form validation & sanitization on the server side

--- app.js
+++ app.js
@@ -17,4 +17,6 @@ app.use((req, res) => {
 	res.status(404).render('404');
 });
 
-app.listen(port, () => console.log(`Server running, listening to port ${port}`));
+app.listen(port, () =>
+	console.log(`Server running, listening to port ${port}`)
+);
--- routes/new.js
+++ routes/new.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const { messages } = require('./index');
+const { body, validationResult } = require('express-validator');
+
+const validationRules = [
+	body('message')
+		.trim()
+		.matches(/^[a-zA-Z0-9\s.,:;!?(){}[\]'"@#$%^&*+=_/\\-]*$/)
+		.withMessage('Only text and punctuation allowed')
+		.escape()
+		.notEmpty()
+		.withMessage('Message is required'),
+	body('author')
+		.trim()
+		.matches(/^[a-zA-Z0-9\s.,:;!?(){}[\]'"@#$%^&*+=_/\\-]*$/)
+		.withMessage('Only text and punctuation allowed')
+		.escape()
+		.notEmpty()
+		.withMessage('Author is required'),
+];
 
 const router = express.Router();
 
 router.get('/new', (req, res) => {
-	res.status(200).render('new');
+	if (req.query.errors) {
+		const errors = JSON.parse(req.query.errors);
+		res.render('new', { errors });
+	} else res.status(200).render('new');
 });
 
-router.post('/new', (req, res) => {
-	const newMessage = {
-		text: req.body.message,
-		user: req.body.author,
-		added: new Date(),
-	};
+router.post('/new', validationRules, (req, res) => {
+	const validationErrors = validationResult(req);
+	if (validationErrors.isEmpty()) {
+		const newMessage = {
+			text: req.body.message,
+			user: req.body.author,
+			added: new Date(),
+		};
 
-	messages.push(newMessage);
-	res.status(201).redirect('/');
+		messages.push(newMessage);
+		res.status(201).redirect('/');
+	} else {
+		const errors = validationErrors.array().map((error) => error.msg);
+		res.redirect(
+			`/new?errors=${encodeURIComponent(JSON.stringify(errors))}`
+		);
+	}
 });
 
 module.exports = router;
